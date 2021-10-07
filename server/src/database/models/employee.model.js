798ffb4df54512bd9663748fa6f5563a717e4937
jahsirtg@protonmail.com <jahsirtg@protonmail.com>
2021-10-07 21:47:20 +0300

    Add database model for Employee

---
+++ server/src/database/models/employee.model.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const employeeSchema = new mongoose.Schema({
+  fname: {
+    type: String,
+    required: true,
+  },
+
+  lname: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+
+  empId: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+
+  jobTitle: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+
+  dept: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+},
+{
+  timestamps: true,
+});
+
+const EmployeeModel = mongoose.model('Admin', employeeSchema);
+
+module.exports = EmployeeModel;
