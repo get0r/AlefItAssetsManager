9849af092ae9c94b1c37327be545a8fa5c2e97a4
jahsirtg@protonmail.com <jahsirtg@protonmail.com>
2021-10-09 09:34:02 +0300

    Add Service function for creating deleting and retrieving of categories.

---
+++ server/src/services/category.service.js
@@ -0,0 +1,75 @@
+const CategoryModel = require('../database/models/category.model');
+
+/**
+ * a method to create a new category record in the database.
+ * @param {String} name category name,
+ * @param {String} desc category description
+ * @returns savedCategory if saving to db successful or null if not
+ */
+const createCategory = async (name, desc) => {
+  const doesCategoryExist = await CategoryModel.findOne({ name }).lean();
+
+  if (doesCategoryExist) {
+    return null;
+  }
+  const newCategory = await new CategoryModel({
+    name,
+    desc,
+  }).save();
+
+  return newCategory;
+};
+
+/**
+ * a method to retrieve a category by its id.
+ * @param {String} categoryId category id
+ * @returns retrieved category- might be empty
+ */
+const getCategoryById = async (categoryId) => {
+  if (categoryId.length !== 24) return null;
+
+  const category = await CategoryModel.findById(categoryId).lean();
+
+  if (!category) {
+    return null;
+  }
+
+  return category;
+};
+
+/**
+ * a method to retrieve all categories.
+ * @returns retrieved categories- might be empty
+ */
+const getCategoriesByUserId = async () => {
+  const categories = await CategoryModel.find().lean();
+  if (!categories) {
+    return [];
+  }
+  return categories;
+};
+
+/**
+ * a method to delete a category record from db using its id.
+ * @param {String} categoryId category id
+ * @returns removed category- might be empty
+ */
+const removeCategoryById = async (categoryId) => {
+  const toBeRemoved = await getCategoryById(categoryId);
+  if (!toBeRemoved) {
+    return null;
+  }
+  const removedCategory = await CategoryModel.deleteOne({ _id: categoryId }).lean();
+  if (!removedCategory) {
+    return null;
+  }
+
+  return toBeRemoved;
+};
+
+module.exports = {
+  createCategory,
+  getCategoryById,
+  getCategoriesByUserId,
+  removeCategoryById,
+};
